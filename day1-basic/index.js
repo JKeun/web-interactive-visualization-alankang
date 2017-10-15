8a93093ae61e22e5c4808511fb07f362110a6578
JKeun <jkeun1224@gmail.com>
2017-10-15 19:41:05 +0900

    add 1day

--- day1-basic/index.js
+++ day1-basic/index.js
@@ -5,4 +5,13 @@ function render() {
     var sec = now.getSeconds();
     var text = 'What time is it now? : ' + hour + ":" + min + ":" + sec;
     window.document.querySelector('p').innerHTML = text;
+
+    var degSec = sec / 60 * 360;
+    document.querySelector("#sec").style.transform = 'rotate(' + degSec + 'deg)';
+    var degMin = min / 60 * 360;
+    document.querySelector("#min").style.transform = 'rotate(' + degMin + 'deg)';
+    var degHour = hour / 12 * 360;
+    document.querySelector("#hour").style.trnsform = 'rotate(' + degHour + 'deg)';
+    window.setInterval(render, 1000);
 }
+
